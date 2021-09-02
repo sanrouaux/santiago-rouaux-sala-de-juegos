80be3cd02a62fdeefa64cf914e057c6b3b5b7005
santiago rouaux <sanrouaux@hotmail.com>
2021-09-02 20:34:01 -0300

    Prepara Home, favicon y nuevos componentes para sala de juegos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { QuienSoyComponent } from './quien-soy/quien-soy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    QuienSoyComponent
   ],
   imports: [
     BrowserModule,
