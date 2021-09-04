0d6b5c0e3083ab77309a4b1b0a376d51eb792835
santiago rouaux <sanrouaux@hotmail.com>
2021-09-04 12:21:19 -0300

    Add Login component template

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private routes:Router) { }
 
   ngOnInit(): void {
+  }
+
+  siguiente() {
+    this.routes.navigate(['home']); 
   }
 
 }
