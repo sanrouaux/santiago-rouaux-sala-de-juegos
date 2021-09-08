b31380a65cc440241212abd5295445bbcd3fc304
santiago rouaux <sanrouaux@hotmail.com>
2021-09-08 10:46:43 -0300

    Add firebase and angular/fire dependencies and initialize AngularFireModule in the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularFireModule } from "@angular/fire/compat";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,7 +18,15 @@ import { QuienSoyComponent } from './quien-soy/quien-soy.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyAFiM9ZiHjnhDYLkctaH4V4JYB03fwd5QQ",
+      authDomain: "santiago-rouaux-sala-de-juegos.firebaseapp.com",
+      projectId: "santiago-rouaux-sala-de-juegos",
+      storageBucket: "santiago-rouaux-sala-de-juegos.appspot.com",
+      messagingSenderId: "905050707701",
+      appId: "1:905050707701:web:21331b5bc46f667f02d7bc"
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
