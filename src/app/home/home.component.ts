160697b8c5f8ca09a99e52f3924ef2e82349c44d
santiago rouaux <sanrouaux@hotmail.com>
2021-09-09 16:10:18 -0300

    Check if user is logged in and redirect to login page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(private routes:Router, private firebaseService : FirebaseService) { }
 
   ngOnInit(): void {
+    if(localStorage.getItem('user') == null) {
+      this.routes.navigate(['login']);
+    }
   }
-
+  
 }
